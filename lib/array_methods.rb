33c2d80d72102a8bb5041e78cf7b97d19c6ce4fd
Moi Petitnoel <moivp4@gmail.com>
2020-02-13 19:34:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,10 @@
 def find_element_index(array, value_to_find)
-  array.
-    
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+    nil
+  end  
 end
 
 def find_max_value(array)
