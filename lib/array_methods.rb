beab61d4780282ac62620aa427e550c598740e71
Moi Petitnoel <moivp4@gmail.com>
2020-02-13 19:49:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,12 +3,18 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       return count
     end
-  end  
+  end
+  nil
 end
 
 def find_max_value(array)
+  x = array[0]
   array.length.times do |count|
-    if array[count] 
+    if array[count] > x
+      x = array[count]
+    end
+  x  
+  end  
 end
 
 def find_min_value(array)
